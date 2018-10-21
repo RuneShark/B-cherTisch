16604f1f716ed46144d0a07f7341df8874d2b3a1
RuneShark <kundo.kenan@gmail.com>
2018-10-21 15:55:32 +0200

    inputsource not finished

--- src/main/java/BTP.java
+++ src/main/java/BTP.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
+import org.jdom2.*;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.XMLOutputter;
+import org.xml.sax.InputSource;
+
 public class BTP {
     public static void main (String[] args) {
-        System.out.println("START");
-        
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("gimme de articles");
+        String output = scanner.nextLine();
+        System.out.println("End " + output);
+
+        SAXBuilder saxBuilder = new SAXBuilder();
+        InputSource inputSource = new InputSource();
     }
 }
